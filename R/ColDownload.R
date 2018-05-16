063d9a9b82da99d24811b0e33e394f476321d75d
Hayes <mhayes1@uwyo.edu>
2018-05-16 08:42:41 -0600

    deal with new or old ATS Transmission files

--- R/ColDownload.R
+++ R/ColDownload.R
@@ -135,19 +135,28 @@ ColDownload<-function(username="",password="",dirdown="",cType='ATS/IRID'){
     gf<-rvest::submit_form(jp,f2[[1]],'ctl00$ContentPlaceHolder1$DownloadAll4',httr::write_disk(d1P,overwrite=T))
 
     
-    # con <- file(d1P,"r")
-    # first_line <- readLines(con,n=1)
-    # close(con)
-    # 
-    # spl<-unlist(strsplit(first_line,split=','))
-    # 
+    con <- file(d1P,"r")
+    first_line <- readLines(con,n=1)
+    close(con)
+
+    spl<-unlist(strsplit(first_line,split=','))
+
+    if(TRUE %in% grepl('Latitude,spl')){
     d2<-read.table(d1P,stringsAsFactors = F,sep=',',fill=T,
                    col.names=paste('column',1:18,sep='_'))
 
     d2<-d2[-1,c(1:15)]
+
+    }
+    if(FALSE %in% grepl('Latitude,spl')){
+      d2<-read.table(d1P,stringsAsFactors = F,sep=',',fill=T,
+                     col.names=paste('column',1:16,sep='_'))
+      
+      d2<-d2[-1,-16]
+
+    }
     names(d2)<-c('Serial','TelemDate','NumFixes','BattVoltage','Mortality','BreakOff','GPSOnTime','SatOnTime',
                  'SatErrors','GMTOffset','LowBatt','Event1','Event2','Event3','Event4')
-
 
     dd<-d2[,1:11]
 
