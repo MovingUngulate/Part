26f08647553155b87c6d4dbdb7bfdf9c66cc1111
Hayes <mhayes1@uwyo.edu>
2018-05-17 22:57:56 -0600

    how many in birth?

--- R/FawnMark.R
+++ R/FawnMark.R
@@ -342,7 +342,8 @@ FawnMark<-function(vecpath,ATSUsers,ATSPass,tempdir,
 
   
   jj<-readRDS(paste0(tempdir,'datastore.RDS'))
-  sub<-as.numeric(table(jj$CurrentVitStatus=='expelled'|jj$CurrentVitStatus=='Birth triggered by light and temperature'|
+  nr<-nrow(jj)
+  sub<-nr-as.numeric(table(jj$CurrentVitStatus=='expelled'|jj$CurrentVitStatus=='Birth triggered by light and temperature'|
                      jj$CurrentVitStatus=='Birth triggered by temperature'|jj$CurrentVitStatus=='Birth triggered by light'|
                      jj$CurrentVitStatus=='Birth triggered by lack of comm'))
   
